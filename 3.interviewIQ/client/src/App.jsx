992d32d621ff6839c2416d7b81f6c0e780514c0f
Shivang Agarwal <shivangagarwal184@gmail.com>
2026-07-11 14:22:39 +0530

    Update App.jsx

--- 3.interviewIQ/client/src/App.jsx
+++ 3.interviewIQ/client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "http://localhost:8000"
+export const ServerUrl  = "https://interviewiq-ai-2-iyrw.onrender.com"
 
 function App() {
 
